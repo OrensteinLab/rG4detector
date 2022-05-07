691defc12fbeb3ea41416493ea00a3696486a180
maorturn <maorturn@gmail.com>
2022-05-07 16:35:53 +0300

    ...

--- code/PARAMETERS.py
+++ code/PARAMETERS.py
@@ -4,4 +4,6 @@ SCREENER_PATH = "screener/"
 ENSEMBLE_SIZE = 5
 NUM_OF_ENSEMBLE_ITERATIONS = 50
 MOUSE_PATH = "independent_datasets/mouse/Table_S1_mouse.csv"
+MOUSE_TRANSCRIPTOME_PATH = "big_files/GCF_000001635_new.26_GRCm38.p6_rna.fna"
+
 
--- code/calculate_correlation.py
+++ code/calculate_correlation.py
@@ -2,10 +2,12 @@ from tensorflow.keras.models import load_model
 from scipy.stats import pearsonr
 from utils import get_data, get_input_size
 import numpy as np
-from utils import set_data_size
+from utils import set_data_size, one_hot_enc
 import pandas as pd
 from PARAMETERS import *
 from Bio import SeqIO
+import getopt
+import sys
 
 
 def get_rG4detector_human_corr(model, path):
@@ -21,58 +23,48 @@ def get_rG4detector_human_corr(model, path):
     return corr
 
 
-def get_rG4detector_mouse_corr():
+def get_rG4detector_mouse_corr(model, transcriptome_path):
+    data_size = get_input_size(model)
     # params
     output = []
-    data_rsr =  []
+    rts_data = []
+    seqs = []
     data_file = pd.read_csv(MOUSE_PATH)
-    records_dict = SeqIO.to_dict(SeqIO.parse("big_files/GCF_000001635_new.26_GRCm38.p6_rna.fna", "fasta"))
+    records_dict = SeqIO.to_dict(SeqIO.parse(transcriptome_path, "fasta"))
 
     for idx, row in data_file.iterrows():
-        rsr_li = row["Untreated (K+)"] / row['Untreated (Li+)']
-        rsr_na = row["Untreated (K+)"] / row['Untreated (Na+)']
-        rsr_both = row["Untreated (K+)"] / row['Untreated (Li+/Na+)']
+        rts_ratio = row["Untreated (K+)"] / row['Untreated (Li+)']
         transcript = row['Transcript']
         pos = row['RT-stop position']
-        if transcript in records_dict and np.isfinite([rsr_li, rsr_na, rsr_both]).all():
+        if transcript in records_dict and np.isfinite(rts_ratio):
+            seqs.append(row["Sequence upstream of RT stop"])
             seq = records_dict[transcript].seq
-            start = max(0, pos - 30 - (input_length-30)//2)
-            end = min(len(seq), pos + (input_length-30)//2)
-            head_padding = max(0, (input_length-30)//2-pos+30)
+            start = max(0, pos - 30 - (data_size-30)//2)
+            end = min(len(seq), pos + (data_size-30)//2)
+            head_padding = max(0, (data_size-30)//2-pos+30)
             sub_seq = str(seq[start:end])
-            # print(sub_seq)
-            data_rsr["Li"].append(rsr_li)
-            data_rsr["Na"].append(rsr_na)
-            data_rsr["Li-Na"].append(rsr_both)
-
-            sub_mat = one_hot_enc(sub_seq)
-            hot_mat = np.zeros([input_length, 4])
+            rts_data.append(rts_ratio)
+            sub_mat = one_hot_enc(sub_seq, remove_last=False)
+            hot_mat = np.zeros([data_size, 4])
             hot_mat[head_padding:sub_mat.shape[0] + head_padding, :] = sub_mat
             output.append(hot_mat)
-        else:
-            missing_counter += 1
-    print(f'\n total missing or missing data = {missing_counter} of {len(output)+missing_counter}')
     one_hot_mat = np.array(output)
 
-    if ENSEMBLE:
-        pred = np.zeros((len(one_hot_mat), 1))
-        for m in model:
-            pred += m(one_hot_mat).numpy()/len(model)
-    else:
-        pred = model.predict(one_hot_mat,  batch_size=len(one_hot_mat))
+    rts_ratio = np.log(rts_data)
+    scores = get_screener_scores(file_path=SCREENER_PATH + "screener_mouse_preds.csv", y=rts_ratio)
+    pred = np.zeros((len(one_hot_mat), 1))
+    for m in model:
+        pred += m(one_hot_mat).numpy()/len(model)
     pred = pred.reshape(len(pred))
-    for t in ["Li", "Na", "Li-Na"]:
-        data = data_rsr[t]
-        data = np.log(data)
-        corr = pearsonr(pred, data)
-        # corr = spearmanr(pred, data)
-        print(f'{t} correlation = {round(corr[0], 3)}')
-    return round(corr[0], 3)
+    scores["rG4detector"] = pearsonr(pred, rts_ratio)
+    return scores
 
 
-def get_screener_preds(file_path, y):
+def get_screener_scores(file_path, y):
     screener_scores = {}
-    pred = pd.read_csv(file_path, usecols=['cGcC', 'G4H', 'G4NN'], sep="\t")
+    pred = pd.read_csv(file_path, usecols=['description', 'cGcC', 'G4H', 'G4NN'], sep="\t")
+    pred = pred.groupby(['description'], sort=False).max()
+
     labels = y.reshape(len(y))
     for col in pred.columns:
         const = 0
@@ -90,13 +82,24 @@ def calculate_human_correlation(model):
     print("Evaluating human correlation:")
     # get screener scores
     _,  [_, y_test], _ = get_data(DATA_PATH, min_read=2000)
-    scores = get_screener_preds(file_path=SCREENER_PATH + "rg4_seq_preds.csv", y=y_test)
-    scores["rG4detector"] = get_rG4detector_corr(model, DATA_PATH)
+    scores = get_screener_scores(file_path=SCREENER_PATH + "screener_human_preds.csv", y=y_test)
+    scores["rG4detector"] = get_rG4detector_human_corr(model, DATA_PATH)
     for m in scores.keys():
         print(f"{m} Pearson correlation = {round(scores[m],3)}")
 
 
 def calculate_mouse_correlation(model):
+    print("Evaluating mouse correlation:")
+    # get screener scores
+    # rts_data = []
+    # data_file = pd.read_csv(MOUSE_PATH)
+    # for idx, row in data_file.iterrows():
+    #     rts_ratio = row["Untreated (K+)"] / row['Untreated (Li+)']
+    #     rts_data.append(rts_ratio)
+    # scores = get_screener_scores(file_path=SCREENER_PATH + "screener_mouse_preds.csv", y=np.log(rts_data))
+    scores = get_rG4detector_mouse_corr(model, MOUSE_TRANSCRIPTOME_PATH)
+    for m in scores.keys():
+        print(f"{m} Pearson correlation = {round(scores[m],3)}")
 
 
 if __name__ == "__main__":
@@ -104,7 +107,14 @@ if __name__ == "__main__":
     rG4detector = []
     for i in range(ENSEMBLE_SIZE):
         rG4detector.append(load_model(f"{model_path}/model_{i}.h5"))
-    calculate_human_correlation(rG4detector)
+
+    # opts, args = getopt.getopt(sys.argv[1:], 'hma')
+    # for op, val in opts:
+    #     if op == "-h":
+    # calculate_human_correlation(rG4detector)
+        # if op == "-m":
+        #     calculate_mouse_correlation(rG4detector)
+    calculate_mouse_correlation(rG4detector)
 
 
 
