44dd5c28c22b3ab2173fffc8fb3be74d32c72068
Maor Turner <maorturn@post.bgu.ac.il>
2022-07-19 17:29:15 +0300

    ...

--- code/create_screener_fasta.py
+++ code/create_screener_fasta.py
@@ -3,13 +3,14 @@ from PARAMETERS import *
 from utils import get_data
 import numpy as np
 
-if len(sys.argv) < 2:
-    print("Execution: create_screener_fasta.py <output_file_name> [OPTIONAL: <window_size>]")
+if len(sys.argv) < 3:
+    print("Execution: create_screener_fasta.py <data_dir_path> <output_file_name> [OPTIONAL: <window_size>]")
     exit(0)
-output_dir = sys.argv[1]
+data_dir_path = sys.argv[1]
+output_dir = sys.argv[2]
 input_size = 60
-if len(sys.argv) == 3:
-    input_size = int(sys.argv[2])
+if len(sys.argv) == 4:
+    input_size = int(sys.argv[3])
 
 # Read test sequences
 _, [x_test, y_test, _], _ = get_data(DATA_PATH)
