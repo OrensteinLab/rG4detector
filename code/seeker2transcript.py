44a0913460b53ec088716a4bfdd5eb7cfb809be6
Maor Turner <maorturn@post.bgu.ac.il>
2022-07-22 10:35:16 +0300

    ...

--- code/seeker2transcript.py
+++ code/seeker2transcript.py
@@ -1,14 +1,13 @@
+from PARAMETERS import *
 from Bio import SeqIO
 import re
 import pandas as pd
+import sys
 import numpy as np
 import time
 import pickle
 from Bio import pairwise2
-from PARAMETERS import *
-from math import isnan
 
-MATCH_THRESHOLD = 0.9
 
 def get_seeker_df(data_path):
     diagram_col = "Sequence diagram (RTS sites are indicated by asterisks)"
@@ -36,69 +35,117 @@ def get_seeker_df(data_path):
         gene = re.findall("ENSG\d*\.\d+", row[gene_col])[0]
         rg4_seq_df.at[idx, "sequence"] = seq[0][:length]
         rg4_seq_df.at[idx, "gene"] = gene
-    return rg4_seq_df.loc[:, ["gene", "sequence"]]
+    return rg4_seq_df.loc[:, ["chromosome", "gene", "sequence", "strand"]].dropna().reset_index()
 
 
-def get_transcript_dict(fasta_path):
+
+def get_transcript_dict(fasta_path, by_gene=False):
     transcript_dict = SeqIO.to_dict(SeqIO.parse(fasta_path, "fasta"))
     keys_tuple_list = []
     for k in transcript_dict.keys():
-        new_key = re.findall("ENST\d*\.\d+", k)
-        if len(new_key) != 1:
-            print(f"found {len(new_key)} matchs in {k}")
-        else:
-            keys_tuple_list.append((k, new_key))
-    for k, new_key in keys_tuple_list:
-        transcript_dict[new_key[0]] = transcript_dict[k]
-        del transcript_dict[k]
+        new_keys = re.findall("(ENS\w\d*\.\d+)", k)
+        assert len(new_keys) == 2, f"ERROR didnt find tr & gene ID in {k}"
+        keys_tuple_list.append((k, new_keys))
+
+    if by_gene:
+        for k, new_keys in keys_tuple_list:
+            if new_keys[1] in transcript_dict:
+                transcript_dict[new_keys[1]][new_keys[0]] = transcript_dict[k]
+            else:
+                transcript_dict[new_keys[1]] = {new_keys[0]: transcript_dict[k]}
+            del transcript_dict[k]
+
+    else:
+        for k, new_keys in keys_tuple_list:
+            transcript_dict[new_keys[0]] = transcript_dict[k]
+            del transcript_dict[k]
     return transcript_dict
 
 
-def set_transcript_match_dict(rg4_seq_df, transcript_dict):
-    transcript_match_dict = {}
+def sort_tr_by_tsl(tr_list):
+    sorted_tr_list = []
+    for tr in tr_list:
+        pos = 0
+        while pos < len(sorted_tr_list):
+            if tr.tsl < sorted_tr_list[pos].tsl or \
+                    (tr.tsl == sorted_tr_list[pos].tsl and tr.len > sorted_tr_list[pos].len):
+                break
+            pos += 1
+        sorted_tr_list = sorted_tr_list[:pos] + [tr] + sorted_tr_list[pos:]
+    return sorted_tr_list
+
+
+def get_gene_tsl_info(info_path):
+    new_dict = {}
+    with open(info_path, 'rb') as f:
+        chr_dict = pickle.load(f)
+    for c in chr_dict:
+        for s in chr_dict[c]:
+            for tr in chr_dict[c][s]:
+                if tr.gene in new_dict:
+                    new_dict[tr.gene].append(tr)
+                else:
+                    new_dict[tr.gene] = [tr]
+    del chr_dict
+    return new_dict
+
+
+def get_seq_pos(seq, tr):
+    seq_match = re.search(seq, tr)
+    if seq_match is None:
+        return None
+    seq_location = seq_match.span()
+    location_arr = np.zeros(len(tr), dtype=bool)
+    location_arr[seq_location[0]:seq_location[1]] = True
+    return location_arr
+
+
+def set_rG4_locations(rg4_seq_df, fasta_path):
+    # get all transcripts dict
+    transcript_dict = get_transcript_dict(fasta_path, by_gene=True)
+    # get transcripts tsl info
+    tsl_info = get_gene_tsl_info(tsl_info_path)
+
+    # find rG4 locations in prominent transcripts
+    missing_sequences = 0
+    gene_rG4_locations = {}
     for idx, row in rg4_seq_df.iterrows():
-        seq = row["sequence"]
-        for trans_name in row["transcripts"]:
-            if trans_name not in transcript_dict.keys():
+        tr_list = sort_tr_by_tsl(tsl_info[row["gene"]])
+        for i, tr in enumerate(tr_list):
+            location_arr = get_seq_pos(row["sequence"], str(transcript_dict[tr.gene][tr.name].seq))
+            if location_arr is None:
+                if i == len(tr_list) - 1:
+                    missing_sequences += 1
                 continue
-            seq_match = re.search(seq, str(transcript_dict[trans_name].seq))
-            if seq_match is None:
-                continue
-            seq_location = seq_match.span()
-            location_arr = np.zeros(len(transcript_dict[trans_name].seq), dtype=bool)
-            location_arr[seq_location[0]:seq_location[1]] = True
-            if trans_name in transcript_match_dict:
-                transcript_match_dict[trans_name] = transcript_match_dict[trans_name] | location_arr
+            if tr.gene in gene_rG4_locations:
+                if tr.name in gene_rG4_locations[tr.gene]:
+                    gene_rG4_locations[tr.gene][tr.name] = gene_rG4_locations[tr.gene][tr.name] | location_arr
+                else:
+                    gene_rG4_locations[tr.gene][tr.name] = location_arr
             else:
-                transcript_match_dict[trans_name] = location_arr
-            row["transcripts"].remove(trans_name)
+                gene_rG4_locations[tr.gene] = {tr.name: location_arr}
+            rg4_seq_df.loc[idx, "transcript"] = tr.name
             break
+    print(f"missing sequences = {missing_sequences}")
 
     # search for transcripts with multiple sequences
-    for idx, row in rg4_seq_df.iterrows():
-        seq = row["sequence"]
-        for trans_name in row["transcripts"]:
-            if trans_name not in transcript_match_dict:
-                continue
-            seq_match = re.search(seq, str(transcript_dict[trans_name].seq))
-            if seq_match is None:
-                alignments = pairwise2.align.localms(seq, str(transcript_dict[trans_name].seq), 1, -.8, -.8, -.8)
-                if alignments[0].score / len(seq) > MATCH_THRESHOLD:
-                    seq_location = (alignments[0].start, alignments[0].end)
-                else:
-                    continue
-            else:
-                seq_location = seq_match.span()
-            location_arr = np.zeros(len(transcript_dict[trans_name].seq), dtype=bool)
-            location_arr[seq_location[0]:seq_location[1]] = True
-            transcript_match_dict[trans_name] = transcript_match_dict[trans_name] | location_arr
-
-    print(f"total transcripts = {len(transcript_match_dict)}")
-    return transcript_match_dict
+    transcripts_rG4_locations = {}
+    for gene in gene_rG4_locations:
+        gene_df = rg4_seq_df[rg4_seq_df["gene"] == gene]
+        for tr in gene_rG4_locations[gene]:
+            for idx, row in gene_df.iterrows():
+                if row["transcript"] != tr:
+                    location_arr = get_seq_pos(row["sequence"], str(transcript_dict[gene][tr].seq))
+                    if location_arr is None:
+                        continue
+                    gene_rG4_locations[gene][tr] = gene_rG4_locations[gene][tr] | location_arr
+            transcripts_rG4_locations[tr] = gene_rG4_locations[gene][tr]
+    return transcripts_rG4_locations
 
 
-def print_seeker_fasta(transcript_match_dict, transcript_dict):
+def print_seeker_fasta(transcript_match_dict, transcript_dict_path):
     temp_dict = {}
+    transcript_dict = get_transcript_dict(transcript_dict_path)
     for trans_name in transcript_match_dict:
         temp_dict[trans_name] = transcript_dict[trans_name]
         temp_dict[trans_name].id = trans_name
@@ -112,11 +159,10 @@ def main():
     rg4_seq_df = get_seeker_df(RG4_SEEKER_PATH)
     # transcript_dict = get_transcript_dict(HUMAN_TRANSCRIPTOME_PATH)
     test_df = rg4_seq_df[rg4_seq_df["chromosome"] == "chr1"]
-    output_file = DETECTION_RG4_SEEKER_HITS
-    transcript_match_dict = set_transcript_match_dict(test_df, transcript_dict)
-    with open(output_file, "wb") as fp:  # Pickling
+    transcript_match_dict = set_rG4_locations(test_df, HUMAN_V29_TRANSCRIPTOME_PATH)
+    with open(DETECTION_RG4_SEEKER_HITS, "wb") as fp:  # Pickling
         pickle.dump(transcript_match_dict, fp)
-    print_seeker_fasta(transcript_match_dict, transcript_dict)
+    print_seeker_fasta(transcript_match_dict, HUMAN_V29_TRANSCRIPTOME_PATH)
     print(f"Execution time: {round(time.time() - t)}s")
 
 
